58d2bb4ce304521aceef1a76ead931f946e4d21d
seven05 <ock1997@gmail.com>
2024-08-17 20:04:04 +0900

    1715.py complete

--- jungle_week2/2day/1715.py
+++ jungle_week2/2day/1715.py
@@ -4,5 +4,12 @@ N = int(sys.stdin.readline().strip())
 data=[]
 for i in range(N):
     data.append(int(sys.stdin.readline().strip()))
-
-min_heap = []
+heapq.heapify(data)     # 이거 안했다가 4%에서 틀렸습니다. 뜸
+result = 0
+while len(data) > 1 : # 힙 루트에 있는 애가 제일 최소쌍이므로 heappop을 두번해서 더하고 결과에 추가함
+    A = heapq.heappop(data)
+    B = heapq.heappop(data)
+    C = A + B
+    result += C
+    heapq.heappush(data,C)
+print(result)
