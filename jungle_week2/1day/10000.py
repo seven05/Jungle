4325cb376176c078db304e8c387c6a02f9283d88
seven05 <ock1997@gmail.com>
2024-08-17 11:25:46 +0900

    0817 start and 18258.py init

--- jungle_week2/1day/10000.py
+++ jungle_week2/1day/10000.py
@@ -1,8 +1,6 @@
 import sys
 
-N = int(sys.stdin.readline().strip())
+n = int(sys.stdin.readline().strip())
 circles = []
-for i in range(N):
-    circles.append(list(map(int,sys.stdin.readline().split())))
-# print(circles)
+
 
