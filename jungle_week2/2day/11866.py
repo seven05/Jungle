10d9487aac95a0863880f87ad62a8cb5805b176c
seven05 <ock1997@gmail.com>
2024-08-17 15:36:14 +0900

    11866.py complete

--- jungle_week2/2day/11866.py
+++ jungle_week2/2day/11866.py
@@ -2,4 +2,13 @@ import sys
 
 N,K = map(int,sys.stdin.readline().split())
 # print(N,K)
-
+circle = [i for i in range(1,N+1)]
+result = []
+start = K-1
+while len(circle)>1:
+    result.append(circle[start])
+    circle.pop(start)
+    start = (start + K-1)%len(circle)
+result.append(circle[0])
+output = '<' + ', '.join(map(str, result)) + '>'
+print(output)
