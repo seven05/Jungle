ec003524971463413535406c1cb65f5a46dd02a3
seven05 <ock1997@gmail.com>
2024-08-17 20:06:05 +0900

    13334.py init

---
+++ jungle_week2/2day/13334.py
@@ -0,0 +1,10 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline().strip())
+data=[]
+for i in range(N):
+    data.append(list(map(int,sys.stdin.readline().split())))
+D = int(sys.stdin.readline().strip())
+# print(data)
+
