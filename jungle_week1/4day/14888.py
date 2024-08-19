427c0520678d7b826ec3af346bb39e35a5453ae5
seven05 <ock1997@gmail.com>
2024-08-19 19:18:34 +0900

    240819_1918

--- jungle_week1/4day/14888.py
+++ jungle_week1/4day/14888.py
@@ -1,71 +1,35 @@
 import sys
-# from itertools import permutations
+from itertools import permutations
 # 중복제거 + 리스트에 담지않는 최적화 생각해보기
-
 n= int(sys.stdin.readline().strip())
 nums = (list(map(int, sys.stdin.readline().split())))
-add, sub, mul, div = map(int,sys.stdin.readline().split())
-# print(add,sub,mul,div)
-def dfs(i, res, add, sub, mul, div):
-    global max_val, min_val
-    if i == n:
-        max_val = max(max_val, res)
-        min_val = min(min_val, res)
-        return
-    if add:
-        dfs(i + 1, res + nums[i], add - 1, sub, mul, div)
-    if sub:
-        dfs(i + 1, res - nums[i], add, sub - 1, mul, div)
-    if mul:
-        dfs(i + 1, res * nums[i], add, sub, mul - 1, div)
-    if div:
-        if res < 0:
-            dfs(i + 1, -(-res // nums[i]), add, sub, mul, div - 1)
-        else:
-            dfs(i + 1, res // nums[i], add, sub, mul, div - 1)
+symbols_count = list(map(int, sys.stdin.readline().split()))  # 각 연산자의 개수
+symbols = []
+symbols += ['+'] * symbols_count[0]
+symbols += ['-'] * symbols_count[1]
+symbols += ['*'] * symbols_count[2]
+symbols += ['//'] * symbols_count[3]
 
-max_val = -1000000000
-min_val = 1000000000
+# 최댓값과 최솟값 초기화
+max_value = -float('inf')
+min_value = float('inf')
 
-dfs(1, nums[0], add, sub, mul, div)
-
-print(max_val)
-print(min_val)
-
-# symbols =[]
-# for i in range(4):
-#     for j in range(symbol[i]):
-#         symbols.append(i)
-       
-# # print(symbols)
-# # all_num_arrays = list(permutations(num))
-# all_symbols_arrays = list(permutations(symbols))
-# # print(all_num_arrays)
-# # print(all_symbols_arrays)
-# def cal(n1,n2,s):
-#     if s == 0:
-#         return  n1 + n2
-#     elif s == 1:
-#         return  n1 - n2
-#     elif s == 2:
-#         return  n1 * n2
-#     else:
-#         if n1 < 0 :
-#             return (-(abs(n1)//n2))
-#         else:
-#             return n1 // n2
-# result_max=-1000000000
-# result_min=1000000000
-# for i in all_symbols_arrays:
-#     for j in range(N):
-#         temp = num[0]
-#         if N == 2:
-#            result_max,result_min =cal(num[j],num[j+1],i[0])
-#         else:
-#             for k in range(N-1):
-#                 temp = cal(temp,num[k+1],i[k])
-#             result_max = (max(result_max,temp))
-#             result_min = (min(result_min,temp))
-# print(result_max)
-# print(result_min)
-
+# 연산자의 모든 순열에 대해 계산
+for perm in set(permutations(symbols, len(symbols))):
+    result = nums[0]
+    for i in range(n - 1):
+        if perm[i] == '+':
+            result += nums[i + 1]
+        elif perm[i] == '-':
+            result -= nums[i + 1]
+        elif perm[i] == '*':
+            result *= nums[i + 1]
+        elif perm[i] == '//':
+            if result < 0:
+                result = -(-result // nums[i + 1])
+            else:
+                result //= nums[i + 1]
+    max_value = max(max_value, result)
+    min_value = min(min_value, result)
+print(max_value)
+print(min_value)
