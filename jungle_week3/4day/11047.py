d524acb6541975bac12f25066c8590dc46750b2e
seven05 <ock1997@gmail.com>
2024-08-27 12:54:12 +0900

    11047.py complete

--- jungle_week3/4day/11047.py
+++ jungle_week3/4day/11047.py
@@ -1,1 +1,11 @@
 import sys
+N,K = map(int,sys.stdin.readline().split())
+coins = []
+for _ in range(N):
+    coins.append(int(sys.stdin.readline().strip()))
+result = 0
+for coin in reversed(coins):
+    # print(coin)
+    result += K//coin
+    K = K % coin
+print(result)
